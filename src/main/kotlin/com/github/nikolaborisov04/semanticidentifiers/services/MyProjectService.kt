8c05e6f94bdbfd9f01483897780225720f983ebf
GitHub Action <action@github.com>
2026-04-13 19:48:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/nikolaborisov04/semanticidentifiers/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nikolaborisov04/semanticidentifiers/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.nikolaborisov04.semanticidentifiers.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nikolaborisov04.semanticidentifiers.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
